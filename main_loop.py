11ad6894028fabe6502c8825512678420ef5cd9e
Luke M <ldmatzne@syr.edu>
2025-09-05 16:34:00 -0400

    Update main_loop.py
    I changed Main_loop.py to be in line with the initial conditions in terms of the force vector stuff from the previous commit. So we weren't using the force vector as a initial parameter just to use the function when it doesn't need to be there. 
    
    But I made some other changes: I added in a history for the velocity so we could make velocity graphs if we wanted to. Then I also updated the history so it was actually recording the initial conditions in those lists, and not only the updated ones.

--- main_loop.py
+++ main_loop.py
@@ -1,14 +1,26 @@
 import functions as func
 import matplotlib.pyplot as plt
 
-force_vec, vel_vec, pos_vec, C_d, m, dt = func.initial_conditions() #pull initial conditions
+vel_vec, pos_vec, C_d, m, dt = func.initial_conditions() #pull initial conditions
 
 pos_hist_x, pos_hist_y = [],[] #define lists to store position history
+vel_hist_x, vel_hist_y = [], [] #define lists to store velocity history
 KE,PE,TE = [],[],[]
+
+#Putting in the initial values for the first time step
+pos_hist_x.append(pos_vec["pos_x"])
+pos_hist_y.append(pos_vec["pos_y"])
+vel_hist_x.append(vel_vec["vel_x"])
+vel_hist_y.append(vel_vec["vel_y"])
+
+KE.append(func.calculate_KE(vel_vec,m))
+PE.append(func.calculate_PE(pos_vec,m))
+TE.append(KE[-1] + PE[-1])
+
 while pos_vec["pos_y"] >= 0:
-    updated_force = func.update_force(force_vec, vel_vec, C_d, m) #update force vector
-    updated_vel = func.update_vel(force_vec, vel_vec, m, dt)      #update velocity vector
+    updated_force = func.update_force(vel_vec, C_d, m) #update force vector
     updated_pos = func.update_pos(pos_vec, vel_vec, dt)           #update position vector
+    updated_vel = func.update_vel(force_vec, vel_vec, m, dt)      #update velocity vector
 
     pos_vec = updated_pos
     vel_vec = updated_vel
@@ -16,11 +28,14 @@ while pos_vec["pos_y"] >= 0:
     
     pos_hist_x.append(pos_vec["pos_x"])
     pos_hist_y.append(pos_vec["pos_y"])
+    vel_hist_x.append(vel_vec["vel_x"])
+    vel_hist_y.append(vel_vec["vel_y"])
     
     KE.append(func.calculate_KE(vel_vec,m))
     PE.append(func.calculate_PE(pos_vec,m))
     TE.append(KE[-1] + PE[-1])
-
+    
+#Here we can start plotting things for the while loop for any of our variables. 
 fig, axs = plt.subplots(ncols = 2,layout='constrained')
 axs[0].plot(pos_hist_x,pos_hist_y)
 axs[0].set_xlabel('X-position [m]')
